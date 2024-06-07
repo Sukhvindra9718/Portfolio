2a8661a4c221a1d413c8ae0b1457e2e4973527e2
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2024-06-08 01:48:56 +0530

    Porfolio page add

--- Frontend/src/pages/Portfolio.jsx
+++ Frontend/src/pages/Portfolio.jsx
@@ -1,13 +1,54 @@
 import React from 'react'
+import PortfolioCard from '../Component/PortfolioCard'
 
+const portfolioData = [{
+    industry: "Finance",
+    category: "Web development",
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-1.jpg"
+},
+{
+    industry: "Finance",
+    category: "Web development",
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-2.jpg"
+},
+{
+    industry: "Orizon",
+    category: "Web design",
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg"
+}, {
+    industry: "Finance",
+    category: "Web development",
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-1.jpg"
+},
+{
+    industry: "Finance",
+    category: "Web development",
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-2.jpg"
+},
+{
+    industry: "Orizon",
+    category: "Web design",
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg"
+}]
 function Portfolio() {
-  return (
-    <main>
-        <section>
-            
-        </section>
-    </main>
-  )
+    return (
+        <main>
+            <section>
+                <ul>
+                    <li><a href="">All</a></li>
+                    <li><a href="">Web design</a></li>
+                    <li><a href="">Applications</a></li>
+                    <li><a href="">Web development</a></li>
+                </ul>
+            </section>
+            <section>
+                {portfolioData.map((data, index) => (
+                    <PortfolioCard key={index} data={data} />
+                ))
+                }
+            </section>
+        </main>
+    )
 }
 
 export default Portfolio
