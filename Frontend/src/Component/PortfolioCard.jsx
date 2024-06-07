a7aca16b34c97814e5706c793c78978924b9511e
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2024-06-08 03:02:56 +0530

    changes

--- Frontend/src/Component/PortfolioCard.jsx
+++ Frontend/src/Component/PortfolioCard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function PortfolioCard() {
-  return (
-    <div>PortfolioCard</div>
-  )
+function PortfolioCard({ key, data }) {
+    return (
+        <div className='portfolio-card' key={key}>
+            <div className='img-container'>
+                <img src={data.img} alt=""/>
+            </div>
+            <div className='portfolio-card-info'>
+                <h3>{data.industry}</h3>
+                <p>{data.category}</p>
+            </div>
+        </div>
+    )
 }
 
 export default PortfolioCard
--- Frontend/src/pages/Portfolio.jsx
+++ Frontend/src/pages/Portfolio.jsx
@@ -11,7 +11,7 @@ const portfolioData = [{
 {
     industry: "Finance",
     category: "Web development",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-2.jpg"
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-2.png"
 },
 {
     industry: "Orizon",
@@ -25,7 +25,21 @@ const portfolioData = [{
 {
     industry: "Finance",
     category: "Web development",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-2.jpg"
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-5.png"
+},
+{
+    industry: "Orizon",
+    category: "Web design",
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg"
+}, {
+    industry: "Finance",
+    category: "Web development",
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-1.jpg"
+},
+{
+    industry: "Finance",
+    category: "Web development",
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-5.png"
 },
 {
     industry: "Orizon",
@@ -33,17 +47,20 @@ const portfolioData = [{
     img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg"
 }]
 function Portfolio() {
+
+    const [category, setCategory] = React.useState("All")
+
     return (
         <main className='portfolio'>
             <section className='category-header'>
                 <ul>
-                    <li><a href="">All</a></li>
+                    <li><a href="" style={category === "All" ? {color:"#ffdb70"}:{}}>All</a></li>
                     <li><a href="">Web design</a></li>
                     <li><a href="">Applications</a></li>
                     <li><a href="">Web development</a></li>
                 </ul>
             </section>
-            <section>
+            <section className='portfolio-container'>
                 {portfolioData.map((data, index) => (
                     <PortfolioCard key={index} data={data} />
                 ))
