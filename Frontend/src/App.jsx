d20cf9f77329539b6e6b6a585371602cff19f4a4
prateekpandey123 <prateekpandey736@gmail.com>
2024-06-08 17:24:17 +0530

    add navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,14 +3,18 @@ import "./App.css";
 import Sidebar from "../Component/Sidebar";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
+import Navbar from "../Component/Navbar";
 
 function App() {
   const [page, setPage] = React.useState("about");
   return (
     <div className="app">
       <Sidebar />
-      {page === "about" && <About />}
-      {page === "portfolio" && <Portfolio />}
+      <div style={{ position: "relative" }}>
+        {page === "about" && <About />}
+        {page === "portfolio" && <Portfolio />}
+        <Navbar page={page} setPage={setPage} />
+      </div>
     </div>
   );
 }
