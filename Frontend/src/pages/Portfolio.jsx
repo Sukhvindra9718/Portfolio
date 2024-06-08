2c011c20fdf9a3ed9ad173ceae19c9a53cc56473
prateekpandey123 <prateekpandey736@gmail.com>
2024-06-08 13:34:49 +0530

    icon add

--- Frontend/src/pages/Portfolio.jsx
+++ Frontend/src/pages/Portfolio.jsx
@@ -1,73 +1,111 @@
-import React from 'react'
-import PortfolioCard from '../Component/PortfolioCard'
-import "../Component/Portfolio.css";
+import React from "react";
+import PortfolioCard from "../../Component/PortfolioCard";
+import "../../Component/Portfolio.css";
 
-
-const portfolioData = [{
+const portfolioData = [
+  {
     industry: "Finance",
     category: "Web development",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-1.jpg"
-},
-{
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-1.jpg",
+  },
+  {
     industry: "Finance",
     category: "Web development",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-2.png"
-},
-{
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-2.png",
+  },
+  {
     industry: "Orizon",
     category: "Web design",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg"
-}, {
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg",
+  },
+  {
     industry: "Finance",
     category: "Applications",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-1.jpg"
-},
-{
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-1.jpg",
+  },
+  {
     industry: "Finance",
     category: "Web development",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-5.png"
-},
-{
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-5.png",
+  },
+  {
     industry: "Orizon",
     category: "Web design",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg"
-}, {
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg",
+  },
+  {
     industry: "Finance",
     category: "Applications",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-1.jpg"
-},
-{
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-1.jpg",
+  },
+  {
     industry: "Finance",
     category: "Web development",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-5.png"
-},
-{
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-5.png",
+  },
+  {
     industry: "Orizon",
     category: "Web design",
-    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg"
-}]
+    img: "https://codewithsadee.github.io/vcard-personal-portfolio/assets/images/project-3.jpg",
+  },
+];
 function Portfolio() {
+  const [category, setCategory] = React.useState("All");
 
-    const [category, setCategory] = React.useState("All")
-
-    return (
-        <main className='portfolio'>
-            <section className='category-header'>
-                <ul>
-                    <li className={category === "All" ? 'active-category':'non-active-category'} onClick={()=>setCategory("All")}>All</li>
-                    <li className={category === "Web design" ? 'active-category':'non-active-category'} onClick={()=>setCategory("Web design")}>Web design</li>
-                    <li className={category === "Applications" ? 'active-category':'non-active-category'} onClick={()=>setCategory("Applications")}>Applications</li>
-                    <li className={category === "Web development" ? 'active-category':'non-active-category'} onClick={()=>setCategory("Web development")}>Web development</li>
-                </ul>
-            </section>
-            <section className='portfolio-container'>
-                {portfolioData.map((data, index) => (
-                    (category === data.category || category === "All") && <PortfolioCard key={index} data={data} />
-                ))
-                }
-            </section>
-        </main>
-    )
+  return (
+    <main className="portfolio">
+      <section className="category-header">
+        <ul>
+          <li
+            className={
+              category === "All" ? "active-category" : "non-active-category"
+            }
+            onClick={() => setCategory("All")}
+          >
+            All
+          </li>
+          <li
+            className={
+              category === "Web design"
+                ? "active-category"
+                : "non-active-category"
+            }
+            onClick={() => setCategory("Web design")}
+          >
+            Web design
+          </li>
+          <li
+            className={
+              category === "Applications"
+                ? "active-category"
+                : "non-active-category"
+            }
+            onClick={() => setCategory("Applications")}
+          >
+            Applications
+          </li>
+          <li
+            className={
+              category === "Web development"
+                ? "active-category"
+                : "non-active-category"
+            }
+            onClick={() => setCategory("Web development")}
+          >
+            Web development
+          </li>
+        </ul>
+      </section>
+      <section className="portfolio-container">
+        {portfolioData.map(
+          (data, index) =>
+            (category === data.category || category === "All") && (
+              <PortfolioCard key={index} data={data} />
+            )
+        )}
+      </section>
+    </main>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
